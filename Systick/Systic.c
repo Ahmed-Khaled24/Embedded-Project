285e84d1b8406b979b05074d1c1797d1602fa53b
PhilopateerMoheb1 <104172766+PhilopateerMoheb1@users.noreply.github.com>
2022-04-28 23:24:40 +0200

    Update Systic.c

--- Systick/Systic.c
+++ Systick/Systic.c
@@ -13,6 +13,11 @@ void systic_vid1msDelay(){
 	NVIC_ST_CURRENT_R=0;
 	while((NVIC_ST_CTRL_R&0x00010000)==0){};
 }
+void systic_vid1microDelay(){
+	NVIC_ST_RELOAD_R=80-1;
+	NVIC_ST_CURRENT_R=0;
+	while((NVIC_ST_CTRL_R&0x00010000)==0){};
+}
 void systic_vidDelay(unsigned int ms){
 	unsigned int i;
 	for(i=0;i<ms;i++){
