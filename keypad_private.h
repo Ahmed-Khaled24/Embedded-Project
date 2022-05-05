b4a4cd71df4ce077dc0c8525198084f8375dea38
Malek345 <73738469+Malek345@users.noreply.github.com>
2022-05-05 23:49:38 +0200

    KEYPAD DRIVER
    in Keypad.c : have functions as following
    1\ KEYPAD_init >>> to initialize and set pins for rows and colums 
    2\ KeyPad_u8CheckRow >>> to check witch row has the bottom that is pressed
    3\KEYPAD_u8GetButton >>> to get the exact bottom that is pressed after executing (KeyPad_u8CheckRow ) function

---
+++ keypad_private.h
@@ -0,0 +1,23 @@
+/*
+ * Keypad_private.h
+ *
+ * Created: 1/5/2022 
+ *  Author: Malek Abdelrhman
+ * 
+ * 
+ * 
+ */
+
+// FOR FUNCTIONS THAT I DON'T WANT TO USER TO SEE 
+#ifndef KEYPAD_PRIVATE_H_
+#define KEYPAD_PRIVATE_H_
+
+#define KEYPAD_u8INTERNAL	0 
+#define KEYPAD_u8EXTERNAL	1 
+
+
+static u8 KeyPad_u8CheckRow(u8 CopyNumber); 
+
+
+
+#endif 
