6f1aa9d0d1ba54b3368579fc81d035fa2d1a7a34
Ghaith-Bassam-Zaza <104207733+Ghaith-Bassam-Zaza@users.noreply.github.com>
2022-05-15 10:00:27 +0200

    fix
    fixed one time interrupt bug

--- Interrupt/interrupt.c
+++ Interrupt/interrupt.c
@@ -1,7 +1,9 @@
 #include "Interrupt.h"
+uint8_t PN;
+void (*F)(void);
 void Interrupt_init(uint8_t pn)
 {
-    
+    PN = pn;
 	 
 	
 	GPIO_DIO_vidPinInit(GPIO_PORTF,pn);
@@ -9,15 +11,20 @@ void Interrupt_init(uint8_t pn)
 	GPIO_vidSetPinPullUpRes(GPIO_PORTF,pn,1);
     
     /* configure PORTF4, 0 for falling edge trigger interrupt */
-   	GPIOF->IS  &= ~(1<<pn);        /* make bit 4, 0 edge sensitive */
-    	GPIOF->IBE &=~(1<<pn);         /* trigger is controlled by IEV */
-    	GPIOF->IEV &= ~(1<<pn);       /* falling edge trigger */
-    	GPIOF->ICR |= (1<<pn);          /* clear any prior interrupt */
-    	GPIOF->IM  |= (1<<pn);          /* unmask interrupt */
-    
+	CLEAR_BIT(GPIO_PORTF_IS_R,pn);/* make bit 4, 0 edge sensitive */
+  CLEAR_BIT(GPIO_PORTF_IBE_R,pn);/* trigger is controlled by IEV */
+  CLEAR_BIT(GPIO_PORTF_IEV_R,pn);/* falling edge trigger */
+	
+	
+	SET_BIT(GPIO_PORTF_ICR_R,pn);/* clear any prior interrupt */
+  SET_BIT(GPIO_PORTF_IM_R,pn);/* unmask interrupt */
+  
+  
+  
+
     /* enable interrupt in NVIC and set priority to 3 */
-    	NVIC->IP[30] = 3 << 5;     /* set interrupt priority to 3 */
-    	NVIC->ISER[0] |= (1<<30);  /* enable IRQ30 (D30 of ISER[0]) */
+   NVIC->IP[30] = 3 << 5;     /* set interrupt priority to 3 */
+   NVIC->ISER[0] |= (1<<30);  /* enable IRQ30 (D30 of ISER[0]) */
 
     
     
@@ -25,7 +32,12 @@ void Interrupt_init(uint8_t pn)
 
 /* SW1 is connected to PF4 pin, SW2 is connected to PF0. */
 /* Both of them trigger PORTF falling edge interrupt */
-void GPIOF_Handler(void (*f)(void))
+void GPIOF_setHandler(void (*f)(void))
 {	
-	f();
+	F = f;
+}
+void GPIOF_Handler(void)
+{	
+	F();
+	SET_BIT(GPIO_PORTF_ICR_R,PN);/* clear the interrupt flag */
 }
