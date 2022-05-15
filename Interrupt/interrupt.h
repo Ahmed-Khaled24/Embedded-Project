6b5fc915cd0d7b0df0ff7427fb5e236f2161e216
Ghaith-Bassam-Zaza <104207733+Ghaith-Bassam-Zaza@users.noreply.github.com>
2022-05-15 10:02:54 +0200

    update
    updated interface to fit for last c file update

--- Interrupt/interrupt.h
+++ Interrupt/interrupt.h
@@ -2,10 +2,13 @@
 #ifndef INTERRUPT_DRIVER_H
 #define INTERRUPT_DRIVER_H
 
-#include "TM4C123.h"                    // Device header
+               
 #include "GPIO_driver.h"
 #include <stdint.h>
+#include <GPIO\TM4C123GH6PM.h>
+#include "TM4C123.h"
 
 void Interrupt_init(uint8_t pn);// pin number 1 or 4
-void GPIOF_Handler(void (*f)(void));
+void GPIOF_setHandler(void (*f)(void));
+
 #endif
