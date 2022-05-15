16680438559a355ab742fa3e7e424b1fa3deb4d4
Mina Mounir <104218090+Mina-Mounir-Farid@users.noreply.github.com>
2022-05-15 12:11:34 +0200

    fixed prototype for buzzer function

--- Programs/programs.h
+++ Programs/programs.h
@@ -22,5 +22,5 @@ void Program_Finish(); //the finishing program is for LEDs and buzzer in the end
 uint8_t Oven_Ready(); //to check if the door is closed and SW2 is pressed
 void Turn_on_LEDs(); //to turn on the three LEDs in the microcontroller
 void Turn_off_LEDs(); //to turn off the three LEDs in the microcontroller
-void tuneBuzzer(uint8_t u8PortNumberCpy , uint8_t u8PinNumberCpy,uint32_t u32FreqCpy_Hz, uint32_t u32TimeCpy_ms); //a function to use the buzzer which requires the port and pin numbers , the frequency and the time to keep buzzer on
+void tuneBuzzer(void); //a function to use the buzzer 
 #endif // PROGRAMS_H_INCLUDED
