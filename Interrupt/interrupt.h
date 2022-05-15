3a01da712bc29ee9653d6de9b4d683c991892cb8
Ghaith-Bassam-Zaza <104207733+Ghaith-Bassam-Zaza@users.noreply.github.com>
2022-05-15 06:53:37 +0200

    fixes
    added file guard and changed functions names

--- Interrupt/interrupt.h
+++ Interrupt/interrupt.h
@@ -1,6 +1,11 @@
-#include "stdint.h"
-#include "../GPIO/tm4c123gh6pm.h"
 
-void RGB_Init(void);
-void Interrupt_Init(void);
-void GPIOPortF_Handler(void);
+#ifndef INTERRUPT_DRIVER_H
+#define INTERRUPT_DRIVER_H
+
+#include "TM4C123.h"                    // Device header
+#include "GPIO_driver.h"
+#include <stdint.h>
+
+void Interrupt_init(uint8_t pn);// pin number 1 or 4
+void GPIOF_Handler(void (*f)(void));
+#endif
