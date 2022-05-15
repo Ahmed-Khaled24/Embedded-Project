ddb368b5769fcbd51fa237e929a13d5d8989f36a
Ghaith-Bassam-Zaza <104207733+Ghaith-Bassam-Zaza@users.noreply.github.com>
2022-05-15 06:56:53 +0200

    fixes
    fixed bugs and changed handler to function pointer parameter function

--- Interrupt/interrupt.c
+++ Interrupt/interrupt.c
@@ -1,42 +1,31 @@
-#include "stdint.h"
-#include "../GPIO/tm4c123gh6pm.h"
-#include "../Systick/Systick.h"
+#include "Interrupt.h"
+void Interrupt_init(uint8_t pn)
+{
+    
+	 
+	
+	GPIO_DIO_vidPinInit(GPIO_PORTF,pn);
+	GPIO_vidSetPinDirection(GPIO_PORTF,pn,0);
+	GPIO_vidSetPinPullUpRes(GPIO_PORTF,pn,1);
+    
+    /* configure PORTF4, 0 for falling edge trigger interrupt */
+   	GPIOF->IS  &= ~(1<<pn);        /* make bit 4, 0 edge sensitive */
+    	GPIOF->IBE &=~(1<<pn);         /* trigger is controlled by IEV */
+    	GPIOF->IEV &= ~(1<<pn);       /* falling edge trigger */
+    	GPIOF->ICR |= (1<<pn);          /* clear any prior interrupt */
+    	GPIOF->IM  |= (1<<pn);          /* unmask interrupt */
+    
+    /* enable interrupt in NVIC and set priority to 3 */
+    	NVIC->IP[30] = 3 << 5;     /* set interrupt priority to 3 */
+    	NVIC->ISER[0] |= (1<<30);  /* enable IRQ30 (D30 of ISER[0]) */
 
-
-
-unsigned char InterruptsControl;
-
-void Interrupt_Init(void){
-SYSCTL_RCGCGPIO_R |= 0x00000020; // activate clock for port F
-GPIO_PORTF_DIR_R &= ~0x10; // make PF4 in (built-in button)
-GPIO_PORTF_DEN_R |= 0x10; // enable digital I/O on PF4
-GPIO_PORTF_PUR_R |= 0x10; // enable weak pull-up on PF4
-GPIO_PORTF_IS_R &= ~0x10; // PF4 is edge-sensitive
-GPIO_PORTF_IBE_R &= ~0x10; // PF4 is not both edges
-//PortF Interrupt Initialization
-InterruptsControl++;
-GPIO_PORTF_IEV_R |=0x10; // PF4 RAISING edge event
-GPIO_PORTF_ICR_R = 0x10; // clear flag4
-GPIO_PORTF_IM_R |= 0x10; // arm interrupt on PF4
-NVIC_PRI7_R = (NVIC_PRI7_R&0xFF00FFFF)|0x00A00000; // (g) priority 5
-NVIC_EN0_R = 0x40000000; // (h) enable interrupt 30 in NVIC
-EnableInterrupts(); // (i) Enable global Interrupt flag (I)
+    
+    
 }
-void GPIOPortF_Handler(void){
 
-	GPIO_PORTF_ICR_R = 0x10; // acknowledge flag4
-	GPIO_DIO_vidPortInit(GPIO_PORTF); //initialize port F
-	while(1){
-
-		
-		if(InterruptsControl>1){
-		GPIO_PORTF_DATA_R ^= 0x0E;
-		LCD_vidPause();
-		systick_vidDelay(2000);
-		}
-		else if(InterruptsControl==1){
-			//pause(Systic delay) or LCD function
-		
-		}
-	}
+/* SW1 is connected to PF4 pin, SW2 is connected to PF0. */
+/* Both of them trigger PORTF falling edge interrupt */
+void GPIOF_Handler(void (*f)(void))
+{	
+	f();
 }
