b3c1bf9fe43f89911189bf773be278537e9612fa
Mina Mounir <104218090+Mina-Mounir-Farid@users.noreply.github.com>
2022-05-17 16:49:35 +0200

    fixes and updates

--- Programs/programs.c
+++ Programs/programs.c
@@ -3,7 +3,7 @@
 */
 #include "programs.h"
 #include "tm4c123.h"
-void External_Button_Init() 
+void External_Button_Init()
 {
     GPIO_DIO_vidPinInit(GPIO_PORTE , 0);
     GPIO_vidSetPinDirection(GPIO_PORTE, 0, 0); //input
@@ -11,24 +11,31 @@ void External_Button_Init()
 
 }
 
-void Buzzer_Init() 
+void Buzzer_Init()
 {
     GPIO_DIO_vidPinInit(GPIO_PORTB , 3);
     GPIO_vidSetPinDirection(GPIO_PORTB, 3, 1); //output
-    
-} 
-void Program_Interrupt() 
+
+}
+void Program_Interrupt()
 {
+
     while(1) //pause loop
     {
+
+		   SET_BIT(GPIO_PORTF_IS_R,4);
            GPIO_PORTF_DATA_R ^= 0x0E; //blinking leds
-           systick_vidDelay(2000);
+           systick_vidDelay(500);
          if(GPIO_u8GetPinValue(GPIO_PORTF,4) == Low) //if SW1 is pressed again
         {
+			CLEAR_BIT(GPIO_PORTF_IS_R,4);
             NVIC_SystemReset(); //terminate cooking process
          }
-         if(GPIO_u8GetPinValue(GPIO_PORTF,0) == Low && GPIO_u8GetPinValue(GPIO_PORTE,0) == High ) //if SW2 is pressed again resume cooking and door is closed 
-            break;
+         if(GPIO_u8GetPinValue(GPIO_PORTF,0) == Low && GPIO_u8GetPinValue(GPIO_PORTE,0) == High )
+          {//if SW2 is pressed again resume cooking and door is closed
+           CLEAR_BIT(GPIO_PORTF_IS_R,4);
+			break;
+		 }
     }
 }
 void Program_A()
@@ -37,7 +44,7 @@ void Program_A()
      while(Oven_Ready() == 0) ; //wait until oven is ready (door closed and SW2 pressed)
      Turn_on_LEDs() ;
      LCD_vidCountDown(100,1); //wait for 1 minute (input entered in format (1:00)
-		Program_Finish() ; //finsihing  program for microwave
+	Program_Finish() ; //finsihing  program for microwave
 }
 
 void Program_B_or_C(uint8_t key) //this program works for choices B or C
@@ -47,15 +54,21 @@ void Program_B_or_C(uint8_t key) //this program works for choices B or C
 Repeat:  if(key == 'B') //in case keypad button 'B' is pressed ,
         {
             LCD_vidWriteString("Beef Weight?" , strlen("Beef Weight?") ); //then display the beef weight
+            systick_vidDelay(3000);
             defrost_rate = BEEF_DEFROST_RATE; //set the defrost_rate to be 0.5
         }
         else if(key == 'C') //in case keypad button 'C' is pressed ,
         {
             LCD_vidWriteString("Chicken Weight?" , strlen("Chicken Weight?")); //then display the beef weight
+            systick_vidDelay(3000);
             defrost_rate =  CHICKEN_DEFROST_RATE ; //set the defrost_rate to be 0.2
          }
-         kilograms = KEYPAD_u8GetButton(); //the button entered represents the number of kilograms
-        if( kilograms >= 1 && kilograms <=9 )
+         kilograms = '\0'; //the button entered represents the number of kilograms
+
+				while(kilograms =='\0'){
+					kilograms = KEYPAD_u8GetButton();
+				}
+        if( kilograms >= '1' && kilograms <='9' )
 	{
            //any integer between 1 and 9 is accepted
             LCD_vidClearScreen();
@@ -64,27 +77,31 @@ Repeat:  if(key == 'B') //in case keypad button 'B' is pressed ,
             while(Oven_Ready() == 0) ; //must wait until door is closed and SW2 is pressed
             Turn_on_LEDs() ;
             LCD_vidClearScreen();
-            LCD_vidCountDown(defrost_rate * kilograms,0); //time to wait in seconds
+            LCD_vidCountDown(defrost_rate * (kilograms-'0'),0); //time to wait in seconds
 	}
         else{ //invalid number of kilograms
             LCD_vidClearScreen();
             LCD_vidWriteString("Err", strlen("Err")  );
             systick_vidDelay(2000);
+						LCD_vidClearScreen();
             goto Repeat ;
         }
-				Program_Finish() ; //finsihing  program for microwave
+		Program_Finish() ; //finsihing  program for microwave
 }
 
 void Program_D() //for other kinds of food
 {
      uint16_t  timer  ;
      LCD_vidWriteString("Cooking time?", strlen("Cooking time?") );
-
+			LCD_vidClearScreen();
     do{
+				LCD_vidClearScreen();
+				LCD_vidWriteString("(1 till 30:00)",strlen("(1 till 30:00)"));
         timer = LCD_u16TakeInput() ; //user must enter a valid time (1 till 30:00)
+
      }while(timer == 0);
-     if(GPIO_u8GetPinValue(GPIO_PORTF,4) == Low) //switch 1 pressed to clear LCD
-        LCD_vidClearScreen();
+     if(GPIO_u8GetPinValue(GPIO_PORTF,4) == Low)LCD_vidClearScreen(); //switch 1 pressed to clear LCD
+
      while(Oven_Ready() == 0) ; //wait until door is closed switch 2 is pressed then the LEDs are on and the countdown starts
         Turn_on_LEDs() ;
         LCD_vidCountDown(timer,1)  ;
@@ -102,7 +119,7 @@ void Program_Finish() //terminating program of the microwave. this is always exe
             Turn_off_LEDs();
             tuneBuzzer(); //sound the buzzer and wait for 1 second between blinking periods
          }
-    
+
 
 }
 uint8_t Oven_Ready() //to check if door is closed and SW2 is pressed(cooking conditions)
