bec9a1f5905b3215e9cc54e8f978606900a20636
PhilopateerMoheb1 <104172766+PhilopateerMoheb1@users.noreply.github.com>
2022-05-17 13:55:23 +0200

    Update Systick.c

--- Systick/Systick.c
+++ Systick/Systick.c
@@ -9,8 +9,7 @@ void systick_vidInit(void){
 }
 
 void systick_vid1msDelay(){
-	NVIC_ST_RELOAD_R = 16000 - 1;  //delay of 1ms is actually 8000 cycles not 80000 (found by experimenting  that the bus clock frequency of our kit is 8MHz not 80MHz)
-	NVIC_ST_CURRENT_R = 0;
+	NVIC_ST_RELOAD_R = 16000 - 1;  
 	while((NVIC_ST_CTRL_R&0x00010000)==0) ;
 }
 void systick_vid1MicroDelay(){
