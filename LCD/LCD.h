1a7ee0c1f1634d7b461da937196351d1719ef63e
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2022-05-17 19:27:38 +0200

    In LCD.h: Add goToRowColumn function. Change parameters of writeChar and WriteString functions. In LCD_configuration.h: Apply the actual connections to the file.

--- LCD/LCD.h
+++ LCD/LCD.h
@@ -15,16 +15,19 @@ void LCD_vidScreenInit(void);
 void LCD_vidClearScreen(void);
 
 // Write a character on the LCD
-void LCD_vidWriteChar(char c);
+void LCD_vidWriteChar(unsigned char c);
 
 // Write a string on the LCD
-void LCD_vidWriteString(char* string, uint8_t stringSize);
+void LCD_vidWriteString(unsigned char* string, uint8_t stringSize);
 
 // Countdown timer 
 void LCD_vidCountDown(int16_t timer, uint8_t isStandardForm);
 
 // Take 4-digit input from the user and return the input.
 uint16_t LCD_u16TakeInput(void);
+
+// Cursor go to the provided location.
+void goToRowColumn(uint8_t row, uint8_t column);
 
 
 #endif
--- LCD/LCD_configuration.h
+++ LCD/LCD_configuration.h
@@ -5,27 +5,27 @@
 // Define LCD pins numbers in their port.
 #define LCD_PD0		0
 #define LCD_PD1		1
-#define LCD_PD2		2
-#define LCD_PD3		3
+#define LCD_PD2		4
+#define LCD_PD3		5
 #define LCD_PD4		4
 #define LCD_PD5		5
 #define LCD_PD6		6
 #define LCD_PD7		7
-#define LCD_PRS		0
-#define LCD_PE		1
+#define LCD_PRS		4
+#define LCD_PE		5
 
 // Define LCD data pins port-connection.
-#define LCD_PD0_PORT		GPIO_PORTD
-#define LCD_PD1_PORT		GPIO_PORTD
-#define LCD_PD2_PORT		GPIO_PORTD
-#define LCD_PD3_PORT		GPIO_PORTD
-#define LCD_PD4_PORT		GPIO_PORTD
-#define LCD_PD5_PORT		GPIO_PORTD
-#define LCD_PD6_PORT		GPIO_PORTD
-#define LCD_PD7_PORT		GPIO_PORTD
+#define LCD_PD0_PORT		GPIO_PORTB
+#define LCD_PD1_PORT		GPIO_PORTB
+#define LCD_PD2_PORT		GPIO_PORTE
+#define LCD_PD3_PORT		GPIO_PORTE
+#define LCD_PD4_PORT		GPIO_PORTB
+#define LCD_PD5_PORT		GPIO_PORTA
+#define LCD_PD6_PORT		GPIO_PORTA
+#define LCD_PD7_PORT		GPIO_PORTA
 
 // Define LCD RS, E pins port-connection
-#define LCD_PRS_PORT		GPIO_PORTB
+#define LCD_PRS_PORT		GPIO_PORTA
 #define LCD_PE_PORT			GPIO_PORTB
 
 #endif
