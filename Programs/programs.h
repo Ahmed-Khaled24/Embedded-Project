33aa0d0c2f2969e933f4cd15f6237adfd60a5154
Mina Mounir <104218090+Mina-Mounir-Farid@users.noreply.github.com>
2022-05-17 13:02:13 +0200

    Update programs.h
    modified defrost rate to be in seconds

--- Programs/programs.h
+++ Programs/programs.h
@@ -13,17 +13,17 @@
 #include "string.h"
 #include "../Keypad/Keypad_interface.h"
 #include "../Keypad/Keypad_configration.h"
-#define BEEF_DEFROST_RATE 0.5
-#define CHICKEN_DEFROST_RATE 0.2
-void Buzzer_Init(); // to intialize the buzzer
-void External_Button_Init(); // to intialize the push button
-void Program_Interrupt(); //for interrupt conditions
-void Program_A() ; //for popcorn
+#define BEEF_DEFROST_RATE 30
+#define CHICKEN_DEFROST_RATE 12
+void Buzzer_Init(void); // to intialize the buzzer
+void External_Button_Init(void); // to intialize the push button
+void Program_Interrupt(void); //for interrupt conditions
+void Program_A(void) ; //for popcorn
 void Program_B_or_C(uint8_t key); //for beef and chicken
-void Program_D(); //for others
-void Program_Finish(); //the finishing program is for LEDs and buzzer in the end
-uint8_t Oven_Ready(); //to check if the door is closed and SW2 is pressed
-void Turn_on_LEDs(); //to turn on the three LEDs in the microcontroller
-void Turn_off_LEDs(); //to turn off the three LEDs in the microcontroller
+void Program_D(void); //for others
+void Program_Finish(void); //the finishing program is for LEDs and buzzer in the end
+uint8_t Oven_Ready(void); //to check if the door is closed and SW2 is pressed
+void Turn_on_LEDs(void); //to turn on the three LEDs in the microcontroller
+void Turn_off_LEDs(void); //to turn off the three LEDs in the microcontroller
 void tuneBuzzer(void); //a function to use the buzzer 
 #endif // PROGRAMS_H_INCLUDED
