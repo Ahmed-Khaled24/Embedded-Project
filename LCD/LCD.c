4523cf4527349988e25ecca3e7c9b35044bbc3cc
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2022-05-17 19:29:41 +0200

    Implement goToRowColumn function. Create two private functions convertTimerToInt and ValidateInput. Add validation section in TakeInput function.

--- LCD/LCD.c
+++ LCD/LCD.c
@@ -5,6 +5,8 @@
 #include "../Keypad/Keypad_interface.h"
 #include "LCD_configuration.h"
 
+#define validInput 			1
+#define invalidInput 		0 
 
 void LCD_vidSendCommand(uint8_t command) {
 	// Select to write on command register [RS = 0]
@@ -83,7 +85,7 @@ void LCD_vidClearScreen(void) {
 	LCD_vidSendCommand(ClearScreen);
 }
 
-void LCD_vidWriteChar(char c) {
+void LCD_vidWriteChar(unsigned char c) {
 	//set RS for data registers (Read/write) and wait for delay
 	GPIO_vidSetPinValue(LCD_PRS_PORT, LCD_PRS, High);
 	systick_vid1msDelay();
@@ -109,7 +111,7 @@ void LCD_vidWriteChar(char c) {
 	systick_vid1msDelay();
 }
 
-void LCD_vidWriteString(char* string, uint8_t stringSize) {
+void LCD_vidWriteString(unsigned char* string, uint8_t stringSize) {
 	uint8_t i;
 	for (i = 0; i < stringSize; i++) {
 		if (i == 16) LCD_vidSendCommand(GoToSecondLine);
@@ -119,63 +121,110 @@ void LCD_vidWriteString(char* string, uint8_t stringSize) {
 
 // Starts countdown on screen with the timer given
 void LCD_vidCountDown(int16_t timer, uint8_t isStandardForm) {
-// you have to input the time as minutesSeconds, ex: 1234 is 12 minutes 34 seconds , 123 is 1 minutes 23 seconds
-// or as a minutes only format as its converted by the MinToTimerStandard function ex:4.5 becomes 430
+/* 
+	you have to input the time as minutesSeconds, ex: 1234 is 12 minutes 34 seconds , 123 is 1 minutes 23 seconds
+	or as a minutes only format as its converted by the MinToTimerStandard function ex:4.5 becomes 430.
+*/ 
 	int8_t min, sec;
+
 	if (isStandardForm == 0) {
 		timer = ((timer / 60) * 100) + (timer % 60); // Converting the seconds form to standard form needed here.
 	}
 
 	min = timer / 100, sec = (int16_t)timer % 100; 		// divide the lower 2 digits to seconds and upper 2 digits to minutes
 					
-	while (min > -1) { 						// keeps looping till the minutes reach 0
-		while (sec > -1) { 					// keeps looping till the seconds reach 0
+	while (min > -1) { 									// keeps looping till the minutes reach 0
+		while (sec > -1) { 								// keeps looping till the seconds reach 0
 			int8_t temp = min;
+			goToRowColumn(2, 10);						// To count in the bottom-right corner of the screen and keep the already wirtten data unchaged.
 			LCD_vidWriteChar((temp / 10) + '0');
 			LCD_vidWriteChar((temp % 10) + '0');
 			LCD_vidWriteChar(':');
-			if (sec < 10) { // If the seconds are in the units write a 0 before it
+			if (sec < 10) { 							// If the seconds are in the units write a 0 before it
 				LCD_vidWriteChar('0');
 				LCD_vidWriteChar(sec + '0');
 			}
-			else { // If not write the string normally
+			else { 										// If not write the string normally
 				int8_t temp = sec;
 				LCD_vidWriteChar((temp / 10) + '0');
-					LCD_vidWriteChar((temp % 10) + '0');
-
+				LCD_vidWriteChar((temp % 10) + '0');
 			}
 			
-			systick_vidDelay(1000); 					//wait a second to decrement a unit digit in the seconds
-			LCD_vidSendCommand(ClearScreen);
+			systick_vidDelay(1000); 					// wait a second to decrement a unit digit in the seconds			
 			sec--; 
+			goToRowColumn(1, 1);
 		}
-		sec = 59;							// set the seconds to a whole minute
+		sec = 59;										// set the seconds to a whole minute
 		min--;
-	
 	}
 }
 
-// These two functions to implement TakeInput().
+// These four functions to implement TakeInput().
 // 1.
-void printTimer(char* timer); // prototype.
-void printTimer(char* timer) {
+void printTimer(uint8_t* timer) {
 	// Loop 4 times as the timer consist of 4 digits.
 	uint8_t i;
+	goToRowColumn(2 , 10) ;				// To take the input in the bottom-right corner of the screen and keep the already wirtten data unchaged.
 	for (i = 0; i < 4; i++) {
 		if (i == 2)   LCD_vidWriteChar(':');
 		LCD_vidWriteChar(timer[i]);
 	}
 }
 // 2. 
-void shiftTimerLeft(char* timer); // prototype.
-void shiftTimerLeft(char* timer) {
+void shiftTimerLeft(uint8_t* timer) {
 	uint8_t i;
 	for (i = 1; i < 4; i++) {
 		timer[i - 1] = timer[i];
 	}
 }
 
-uint16_t LCD_u16TakeInput(void) {
+// 3.
+uint16_t convertTimerToInt(uint8_t* timer){
+	uint8_t i;
+	uint16_t timerInt = 0;
+	for (i = 0; i < 4; i++) {
+		uint16_t currentDigit = timer[i] - '0';
+		if (i == 0)
+			timerInt += (currentDigit * 1000);
+		else if (i == 1)
+			timerInt += (currentDigit * 100);
+		else if (i == 2)
+			timerInt += (currentDigit * 10);
+		else if (i == 3)
+			timerInt += currentDigit;	
+	}
+	return timerInt;
+}
+
+// 4.
+uint16_t validateInput(uint8_t* timer){
+	// If the user inputed a valid number the function returns it, else it returns 0.	
+	uint8_t i;
+	uint16_t timerInt = 0;	
+	uint16_t temp_validator = 0;
+	
+	// Check for letters.
+	for(i = 0 ; i < 4; i++){
+		if(timer[i] < '0' || timer[i] > '9')
+			return timerInt;	
+	}
+	
+	// Ensure that seconds <= 59.
+	if(timer[2] > '5')
+		return timerInt;
+	
+	// Ensure that timer in range (1 sec to 30 min)
+	temp_validator = convertTimerToInt(timer);
+	if( temp_validator < 1 || temp_validator > 3000)
+		return timerInt;
+	else 
+		timerInt = temp_validator;
+	
+	return timerInt;
+}
+
+
+uint16_t LCD_u16TakeInput(void){
 /* 
 	 This function takes 4-digit (numbers) input from the user for example (1, 2, 3, 4)
 	 Important note(1): user must enter the numbers from left to right 1 -> 2 -> 3 -> 4 
@@ -185,12 +234,11 @@ uint16_t LCD_u16TakeInput(void) {
 */
 		uint16_t timerInt = 0;
 	 	uint8_t i;
-		char currentInput;
+		uint8_t currentInput;
+		uint16_t InputResult; 
+		
 	// Create a timer.
-	char timer[4] = {'0', '0', '0', '0'};
-
-	// Clear the screen to start take input from the user.
-	LCD_vidClearScreen();
+	uint8_t timer[4] = {'0', '0', '0', '0'};
 	
 	// Print the current status of the timer to the screen [ 00:00 ].
 	printTimer(timer);
@@ -206,45 +254,34 @@ uint16_t LCD_u16TakeInput(void) {
 		}
 
 		// Convert pressedButton to char to print on the screen, then add it to the right most cell in the timer.
-		currentInput = pressedButton + '0';
+		currentInput = pressedButton ;
 		timer[3] = currentInput;
 
 		// Print the timer with the new input on the LCD. 
 		// For example the user pressed 1 >>>> screen shows [ 00:01 ]
-		LCD_vidClearScreen();
+		
 		printTimer(timer);
 
 		// Shift the timer digits left to take the next input except for the last iteration.
 		if (i < 3)
 			shiftTimerLeft(timer);
-
-		// Wait for 1 second for a new reaction from the user.
-		systick_vidDelay(1000);
-	}
-
-	// Extract the return value from the timer array.
-	for (i = 0; i < 4; i++) {
-		uint16_t currentDigit = timer[i] - '0';
-		if (i == 0)
-			timerInt += (currentDigit * 1000);
-		else if (i == 1)
-			timerInt += (currentDigit * 100);
-		else if (i == 2)
-			timerInt += (currentDigit * 10);
-		else if (i == 3)
-			timerInt += currentDigit;	
 	}
 	
-	if(timerInt >= 1 && timerInt <= 3000)
-		return timerInt;
-	else 
-		return 0;
+	InputResult = validateInput(timer);
+	return InputResult;
 }
 
 
+// Cursor go to the provided location.
+void goToRowColumn(uint8_t row, uint8_t column) {
+	uint8_t i;
+	LCD_vidSendCommand(ResetCursor);
 
+	if(row == 2) 
+		LCD_vidSendCommand(GoToSecondLine);
 
-
-
+	for (i = 0; i < column-1; i++) 
+		LCD_vidSendCommand(ShiftCursorRight);
+}
 
 
