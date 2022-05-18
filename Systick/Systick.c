e21201f0e55504a1c92b7b07836325f3b5635079
PhilopateerMoheb1 <104172766+PhilopateerMoheb1@users.noreply.github.com>
2022-05-18 19:53:13 +0200

    Adding and fixing some new functions
    systick_Enable(),systick_Disable()

--- Systick/Systick.c
+++ Systick/Systick.c
@@ -12,8 +12,14 @@ void systick_vid1msDelay(){
 	NVIC_ST_RELOAD_R = 16000 - 1;  
 	while((NVIC_ST_CTRL_R&0x00010000)==0) ;
 }
+void systick_Disable(){
+	NVIC_ST_CTRL_R=0;
+}//to disable Systick
+
+void systick_Enable(){NVIC_ST_CTRL_R=0x05;}//TO enable Systic
+
 void systick_vid1MicroDelay(){
-	NVIC_ST_RELOAD_R = 8 - 1;
+	NVIC_ST_RELOAD_R = 16 - 1;
 	NVIC_ST_CURRENT_R = 0;
 	while((NVIC_ST_CTRL_R&0x00010000)== 0) ;
 }
